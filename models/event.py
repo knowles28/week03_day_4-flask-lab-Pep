729eebebd5e8d42542b8370f07b5e2a80ea75835
Jack Knowles <jackmhknowles@gmail.com>
2022-07-07 14:39:16 +0100

    actually fixed date input

--- models/event.py
+++ models/event.py
@@ -1,9 +1,10 @@
 class Event():
     
-    def __init__(self, date, name, guests, room, description):
+    def __init__(self, date, name, description, room, guests):
         self.date = date
         self.name = name
         self.guests = guests
         self.room = room
         self.description = description
+        # self.recurring = recurring ^^ADD TO PARAMETERS
     
--- models/event_list.py
+++ models/event_list.py
@@ -1,10 +1,10 @@
 from models.event import *
 
-event1 = Event("monday","football", 50, "Room A", "Euro 2020 final")
-event2 = Event("tuesday", "tennis", 30, "Room B", "Roland Garros")
-event3 = Event("wednesday", "rugby", 20, "Room C", "Six Nations")
-event4 = Event("thursday", "golf", 10, "Room D", "The Open")
-event5 = Event("friday", "cricket", 5, "Room E", "The Ashes")
+event1 = Event("2022-07-01","football", "Euro 2020 final", "Room A", 50)
+event2 = Event("2022-07-02", "tennis", "Roland Garros", "Room B", 30)
+event3 = Event("2022-07-04", "rugby", "Six Nations", "Room C", 20)
+event4 = Event("2022-07-03", "golf", "The Open", "Room D", 10)
+event5 = Event("2022-0703", "cricket", "The Ashes", "Room E", 5)
 
 events = [event1, event2, event3, event4, event5]
 
